dfeb89e3a4a8261a68d39bee167547cdcc941e78
AvielDez <110438720+AvielDez@users.noreply.github.com>
2022-11-29 20:57:14 -0600

    Change image file path attempt 2

--- js/blogPostData.js
+++ js/blogPostData.js
@@ -3,7 +3,7 @@ const blogPost = [
         id: 14,
         name: 'My new journey as a bootcamp student.',
         date: '2022-11-21',
-        thumbnail: './images/blog-image-05.png',
+        thumbnail: 'images/blog-image-05.png',
         summary: "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.",
         postContent: `
         <h2 class="open-post-h2">How I stay committed to learning</h2>
@@ -33,7 +33,7 @@ const blogPost = [
         id: 13,
         name: 'Blog 13',
         date: '2022-11-21',
-        thumbnail: './images/blog-image-05.png',
+        thumbnail: 'images/blog-image-05.png',
         summary: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         postContent: `
         <h2 class="open-post-h2">How I stay committed to learning</h2>
@@ -63,7 +63,7 @@ const blogPost = [
         id: 12,
         name: 'Blog 12',
         date: '2022-11-21',
-        thumbnail: './images/blog-image-05.png',
+        thumbnail: 'images/blog-image-05.png',
         summary: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         postContent: `
         <h2 class="open-post-h2">How I stay committed to learning</h2>
@@ -93,7 +93,7 @@ const blogPost = [
         id: 11,
         name: 'Blog 11',
         date: '2022-11-22',
-        thumbnail: './images/blog-image-05.png',
+        thumbnail: 'images/blog-image-05.png',
         summary: "I'm excited to start a old learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         postContent: `
         <h2 class="open-post-h2">How I stay committed to learning</h2>
@@ -123,7 +123,7 @@ const blogPost = [
         id: 10,
         name: 'Blog 10',
         date: '2022-11-22',
-        thumbnail: './images/blog-image-05.png',
+        thumbnail: 'images/blog-image-05.png',
         summary: "I'm excited to start a old learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         postContent: `
         <h2 class="open-post-h2">How I stay committed to learning</h2>
@@ -153,7 +153,7 @@ const blogPost = [
         id: 9,
         name: 'Blog 9',
         date: '2022-11-21',
-        thumbnail: './images/blog-image-05.png',
+        thumbnail: 'images/blog-image-05.png',
         summary: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         postContent: `
         <h2 class="open-post-h2">How I stay committed to learning</h2>
@@ -183,7 +183,7 @@ const blogPost = [
         id: 8,
         name: 'Blog 8',
         date: '2022-11-22',
-        thumbnail: './images/blog-image-05.png',
+        thumbnail: 'images/blog-image-05.png',
         summary: "I'm excited to start a old learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         postContent: `
         <h2 class="open-post-h2">How I stay committed to learning</h2>
@@ -213,7 +213,7 @@ const blogPost = [
         id: 7,
         name: 'Blog 7',
         date: '2022-11-22',
-        thumbnail: './images/blog-image-05.png',
+        thumbnail: 'images/blog-image-05.png',
         summary: "I'm excited to start a old learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         postContent: `
         <h2 class="open-post-h2">How I stay committed to learning</h2>
@@ -243,7 +243,7 @@ const blogPost = [
         id: 6,
         name: 'Blog 6',
         date: '2022-11-21',
-        thumbnail: './images/blog-image-05.png',
+        thumbnail: 'images/blog-image-05.png',
         summary: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         postContent: `
         <h2 class="open-post-h2">How I stay committed to learning</h2>
@@ -273,7 +273,7 @@ const blogPost = [
         id: 5,
         name: 'Blog 5',
         date: '2022-11-22',
-        thumbnail: './images/blog-image-05.png',
+        thumbnail: 'images/blog-image-05.png',
         summary: "I'm excited to start a old learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         postContent: `
         <h2 class="open-post-h2">How I stay committed to learning</h2>
@@ -303,7 +303,7 @@ const blogPost = [
         id: 4,
         name: 'Blog 4',
         date: '2022-11-22',
-        thumbnail: './images/blog-image-05.png',
+        thumbnail: 'images/blog-image-05.png',
         summary: "I'm excited to start a old learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         postContent: `
         <h2 class="open-post-h2">How I stay committed to learning</h2>
@@ -333,7 +333,7 @@ const blogPost = [
         id: 3,
         name: 'Blog 3',
         date: '2022-11-22',
-        thumbnail: './images/blog-image-05.png',
+        thumbnail: 'images/blog-image-05.png',
         summary: "I'm excited to start a old learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         postContent: `
         <h2 class="open-post-h2">How I stay committed to learning</h2>
@@ -363,7 +363,7 @@ const blogPost = [
         id: 2,
         name: 'Blog 2',
         date: '2022-11-22',
-        thumbnail: './images/blog-image-05.png',
+        thumbnail: 'images/blog-image-05.png',
         summary: "I'm excited to start a old learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         postContent: `
         <h2 class="open-post-h2">How I stay committed to learning</h2>
@@ -393,7 +393,7 @@ const blogPost = [
         id: 1,
         name: 'Blog 1',
         date: '2022-11-22',
-        thumbnail: './images/blog-image-05.png',
+        thumbnail: 'images/blog-image-05.png',
         summary: "I'm excited to start a old learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         postContent: `
         <h2 class="open-post-h2">How I stay committed to learning</h2>
@@ -423,7 +423,7 @@ const blogPost = [
         id: 0,
         name: 'Blog 0',
         date: '2022-11-22',
-        thumbnail: './images/blog-image-05.png',
+        thumbnail: 'images/blog-image-05.png',
         summary: "I'm excited to start a old learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         postContent: `
         <h2 class="open-post-h2">How I stay committed to learning</h2>
